bdf1992187b003767369f6b101a5a30185e44e93
VIVEK DHAKED <38081924+VIVEKDH@users.noreply.github.com>
2020-10-25 20:59:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+const https=require("https");
+
+app.get('/',function(req,res){
+    const url="https://api.openweathermap.org/data/2.5/weather?q=bharatpur,india%20india&appid=79b1455751d63c15bc51c3ed8836ba77";
+    https.get(url,function(response){
+        console.log(response.statusCode);
+    });
+    res.send("server is up and running.");
+});
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("app is listening on 3000");
+});
